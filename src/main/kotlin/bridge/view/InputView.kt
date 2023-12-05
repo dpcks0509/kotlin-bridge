e67f7144cfef7afa1ba359fc8453073cb90b7571
dpcks0509 <dpcks0509@gmail.com>
2023-12-06 01:55:09 +0900

    fix: 입력 예외처리 후 재입력 오류 수정
    사용자가 잘못된 값을 입력할 경우 IllegalArgumentException를 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -10,18 +10,33 @@ class InputView {
         println("다리의 길이를 입력해주세요.")
         val bridgeSize = Console.readLine()
         println()
-        return validateBridgeSize(bridgeSize)
+        return try {
+            validateBridgeSize(bridgeSize)
+        } catch (exception: IllegalArgumentException) {
+            println(exception.message)
+            readBridgeSize()
+        }
     }
 
     fun readMoving(): String {
         println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
         val moving = Console.readLine()
-        return validateMoving(moving)
+        return try {
+            validateMoving(moving)
+        } catch (exception: IllegalArgumentException) {
+            println(exception.message)
+            readMoving()
+        }
     }
 
     fun readGameCommand(): String {
         println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
         val gameCommand = Console.readLine()
-        return validateGameCommand(gameCommand)
+        return try {
+            validateGameCommand(gameCommand)
+        } catch (exception: IllegalArgumentException) {
+            println(exception.message)
+            readGameCommand()
+        }
     }
 }
