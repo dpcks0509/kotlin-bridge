e54087bf831bada476816b625194cf6a67b88afb
dpcks0509 <dpcks0509@gmail.com>
2023-12-06 02:17:25 +0900

    test: 이동할 칸 입력 테스트 추가
    U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력할 수 있으며 올바른 값이 아니면 예외 처리한다.

--- src/test/kotlin/bridge/BridgeTest.kt
+++ src/test/kotlin/bridge/BridgeTest.kt
@@ -1,6 +1,7 @@
 package bridge
 
 import bridge.util.Validator.validateBridgeSize
+import bridge.util.Validator.validateMoving
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -9,13 +10,25 @@ import org.junit.jupiter.params.provider.ValueSource
 class BridgeTest {
     @ParameterizedTest
     @ValueSource(strings = ["3", "12", "20"])
-    fun `올바른 생성할 다리 길이 입력`(bridgeSize: String){
+    fun `올바른 생성할 다리 길이 입력`(bridgeSize: String) {
         assertDoesNotThrow { validateBridgeSize(bridgeSize) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["-1", "0", "2", "21", "3 ", " 20", " ", "", "three"])
-    fun `생성할 다리 길이 입력 예외 처리 (3 이상 20 이하의 숫자가 아닌경우)`(bridgeSize: String){
+    fun `생성할 다리 길이 입력 예외 처리 (3 이상 20 이하의 숫자가 아닌경우)`(bridgeSize: String) {
         assertThrows<IllegalArgumentException> { validateBridgeSize(bridgeSize) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["U", "D"])
+    fun `올바른 이동할 칸 입력`(moving: String) {
+        assertDoesNotThrow { validateMoving(moving) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["u", "d", "Up", "Down", "U ", " D", "1", "0", " ", ""])
+    fun `이동할 칸 입력 예외 처리 (U(위 칸)와 D(아래 칸) 중 하나의 문자가 아닌경우)`(moving: String) {
+        assertThrows<IllegalArgumentException>{ validateMoving(moving) }
     }
 }
